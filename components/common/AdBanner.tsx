f5957312edd7de320cf95313075242dd0570b1f7
Rabinkin Dzmitry <ryabinkin.d@fasttech.dev>
2023-07-04 11:11:07 +0300

    feat: google ads

--- components/common/AdBanner.tsx
+++ components/common/AdBanner.tsx
@@ -1,34 +1,71 @@
+'use client';
 import { ClassAttributes, InsHTMLAttributes, useEffect} from 'react';
+// import {Adsense} from "@ctrl/react-adsense";
+// import Script from "next/script";
+// import dynamic from "next/dynamic";
+import {useRouter} from "next/router";
+import {Adsense} from "@ctrl/react-adsense";
+
+// const Adsense = dynamic(
+//     () => import('@ctrl/react-adsense').then(module=>module.Adsense),
+// {ssr: false}
+// );
+
+
 
 const AdBanner = (props: JSX.IntrinsicAttributes & ClassAttributes<HTMLModElement> & InsHTMLAttributes<HTMLModElement>) => {
-    useEffect(() => {
-        try {
-            (window.adsbygoogle = window.adsbygoogle || []).push({});
-        } catch (err) {
-            console.log(`And failed ${err}`);
-        }
-    }, []);
+    const { isReady } = useRouter();
+
+    //
+    // useEffect(() => {
+    //     if (!isReady) {
+    //         return void 0;
+    //     }
+    //     try {
+    //         (window.adsbygoogle = window.adsbygoogle || []).push({});
+    //     } catch (err) {
+    //         console.log(`And failed ${err}`);
+    //     }
+    // }, [isReady]);
 
     return (
-        <div id="block">
-            <script async
-                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9216561737791732"
-                    crossOrigin="anonymous"></script>
-            <ins
-
-                className="adsbygoogle adbanner-customize"
-                style={{
-                    display:'inline-block',
-                    width:'728px',
-                    height:'90px',
-                    overflow: 'hidden',
-                }}
-                data-ad-client={'ca-pub-9216561737791732'}
-                data-ad-slot="5843715081"
+        <>
+            <Adsense
+                client="ca-pub-9216561737791732"
+                slot="5843715081"
+                style={{ display: 'block' }}
+                layout="in-article"
+                format="fluid"
                 {...props}
             />
-        </div>
+            {/*<Script async*/}
+            {/*        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9216561737791732"*/}
+            {/*        crossOrigin="anonymous"*/}
+            {/*        strategy="lazyOnload"*/}
+            {/*        />*/}
+            {/*<div id="block">*/}
+                <ins
+                    className="adsbygoogle adbanner-customize"
+                    style={{
+                        display:'inline-block',
+                        width:'728px',
+                        height:'90px',
+                        overflow: 'hidden',
+                    }}
+                    data-ad-client={'ca-pub-9216561737791732'}
+                    data-ad-slot="5843715081"
+                    data-ad-format="auto"
+                    data-full-width-responsive="true"
+                    {...props}
+                />
+            {/*</div>*/}
+</>
+
 
     );
 };
-export default AdBanner;
+
+export default AdBanner
+
+
+
---
+++ pages/_document.tsx
@@ -0,0 +1,23 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document'
+import Script from "next/script";
+
+export default class MyDocument extends Document {
+    render() {
+        return (
+            <Html lang='en'>
+                <Head>
+                    <Script
+                        async
+                        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9216561737791732"
+                        strategy="lazyOnload"
+                        crossOrigin="anonymous"
+                    />
+                </Head>
+                <body>
+                <Main />
+                <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
